99e461c7c37f9f5fbb446416d92879b5358af831
AzkaHussein <mochammad_iskandar_33rpl@student.smktelkom-mlg.sch.id>
2025-12-10 19:59:52 +0700

    Create hard.py

---
+++ hard.py
@@ -0,0 +1,5 @@
+def is_palindrome(s): return s == s[::-1]
+
+print(is_palindrome("racecar"))   # Expected: True
+print(is_palindrome("python"))    # Expected: False
+print(is_palindrome("habibah"))   # Expected: True
