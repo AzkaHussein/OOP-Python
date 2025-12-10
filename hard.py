85fe3e87efb4b25e5e47ede3f5bc742a8e60da74
AzkaHussein <mochammad_iskandar_33rpl@student.smktelkom-mlg.sch.id>
2025-12-10 20:01:35 +0700

    Update hard.py

--- hard.py
+++ hard.py
@@ -3,3 +3,12 @@ def is_palindrome(s): return s == s[::-1]
 print(is_palindrome("racecar"))   # Expected: True
 print(is_palindrome("python"))    # Expected: False
 print(is_palindrome("habibah"))   # Expected: True
+
+
+def is_armstrong(n):
+    digits = str(n)
+    return n == sum(int(d)**len(digits) for d in digits)
+
+print(is_armstrong(153))   # Expected: True
+print(is_armstrong(370))   # Expected: True
+print(is_armstrong(123))   # Expected: False
